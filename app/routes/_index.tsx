f1f4b4b7993f0d5faa220cd969b1236bf1f93832
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-31 23:39:18 -0600

    landing page - 02

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { useEffect, useState } from "react";
 import LandingPage from "~/components/LandingPage";
 import mainLandingPageAsset from "~/assets/AstroAssets.png";
 
@@ -7,13 +8,42 @@ import mainLandingPageAsset from "~/assets/AstroAssets.png";
 // };
 
 export default function Index() {
+  const [topButtonDisplay, setTopButtonDisplay] = useState(false);
+
+  const handleScroll = () => {
+    const scrollHeigth = window.scrollY;
+    const screenHeigth = window.innerHeight;
+    setTopButtonDisplay(scrollHeigth > screenHeigth * 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  console.log(topButtonDisplay);
+
   return (
-    <article className="overflow-y-scroll overflow-x-hidden w-auto h-auto">
+    <article className="overflow-y-scroll overflow-x-hidden relative">
+      <div>
+        <LandingPage />
+        <img
+          src={mainLandingPageAsset}
+          alt="Purple Astro riding a bike - Kinxori landing page asset"
+          className={`border-white border fixed transition-all duration-[1s] 
+          top-[0%] left-[40%] w-[1000px] h-[1000px] object-contain 
+          ${topButtonDisplay ? "opacity-0" : "opacity-100"} `}
+        />
+      </div>
+
       <LandingPage />
-      <img
-        src={mainLandingPageAsset}
-        alt="Purple Astro riding a bike - Kinxori landing page asset"
-      />
+      <LandingPage />
+
+      <LandingPage />
       <LandingPage />
     </article>
   );
