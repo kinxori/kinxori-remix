2c65efff2a67cd2cf867fb72638ceef9c1909dab
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-01 20:22:32 -0600

    adding calendly to slide

--- app/components/NavBar/MobileNavBar.tsx
+++ app/components/NavBar/MobileNavBar.tsx
@@ -50,14 +50,14 @@ export default function MobileNav() {
       >
         <h2 className="font-bold text-[30px] font-[rubik] text-white  ">Welcome to Kinxori!</h2>
         <hr className="border-[1px] border-white w-[100%] rounded-lg"></hr>
-        <div className="my-5 flex flex-col gap-3">
+        <div className="my-5 flex flex-col gap-3 items-end">
           <Link
             className={`text-white font-[inter] text-[16px] `}
             //   variant="ghostButton"
             to="/"
             onClick={() => setCurrentPath("/")}
           >
-            Home <i className="fa-solid fa-house"></i>
+            Home Page <i className="fa-solid fa-house"></i>
           </Link>
           <Link
             className={`text-white font-[inter] text-[16px] `}
@@ -65,7 +65,7 @@ export default function MobileNav() {
             to="/"
             onClick={() => setCurrentPath("/")}
           >
-            Home <i className="fa-solid fa-house"></i>
+            About Page <i className="fa-solid fa-house"></i>
           </Link>
           <Link
             className={`text-white font-[inter] text-[16px] `}
@@ -73,19 +73,51 @@ export default function MobileNav() {
             to="/"
             onClick={() => setCurrentPath("/")}
           >
-            Home <i className="fa-solid fa-house"></i>
+            Contact Page <i className="fa-solid fa-house"></i>
           </Link>
+          <a
+            className={`text-white font-[inter] text-[16px] `}
+            target="_blank"
+            href="https://calendly.com/gustavoq26/30min"
+            onClick={() => setCurrentPath("/")}
+          >
+            Schedule a Meeting <i className="fa-solid fa-house"></i>
+          </a>
         </div>
 
         <hr className="border-[1px] border-white w-[100%] rounded-lg mt-auto"></hr>
-        <div className="flex">
+        <div className="flex gap-3">
           <a
             className="w-[30px] h-[30px] text-mainColor bg-white rounded-full flex justify-center items-center"
-            href="/"
+            href="https://linkedin.com/in/quinchori"
             target="_blank"
             onClick={handleSlide}
           >
             <i className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a
+            className="w-[30px] h-[30px] text-mainColor bg-white rounded-full flex justify-center items-center"
+            href="https://github.com/kinxori"
+            target="_blank"
+            onClick={handleSlide}
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            className="w-[30px] h-[30px] text-mainColor bg-white rounded-full flex justify-center items-center"
+            href="https://twitter.com/kinxori"
+            target="_blank"
+            onClick={handleSlide}
+          >
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+          <a
+            className="w-[30px] h-[30px] text-mainColor bg-white rounded-full flex justify-center items-center"
+            href=""
+            target="_blank"
+            onClick={handleSlide}
+          >
+            <i className="fa-regular fa-envelope"></i>
           </a>
         </div>
       </div>
