5e6470024cf70e6dd9039516a71b0741f554548f
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-02 16:04:54 -0600

    adding CustomButton components to repo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import LandingPage from "~/components/LandingPage";
+import LandingPage from "~/components/Pages/LandingPage";
 import mainLandingPageAsset from "~/assets/AstroAssets.png";
 
 export default function Index() {
