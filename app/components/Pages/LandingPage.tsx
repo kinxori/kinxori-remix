3034bda7c8365d14a670535710aea1b9e57b4e6f
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-02 20:33:48 -0600

    updating content - landing page - 05

--- app/components/Pages/LandingPage.tsx
+++ app/components/Pages/LandingPage.tsx
@@ -6,26 +6,26 @@ import Button from "../CustomButton/Button";
 export default function LandingPage({ id }: { id: string }) {
   return (
     <article id={id} className="h-[100vh] w-[100%] mx-auto box-border p-10 overflow-hidden">
-      <div className="h-[100%] w-[100%] flex flex-col justify-center items-center">
-        <div className="flex w-[100%] flex-wrap justify-around text-[60px] ">
-          <h1 className="text-white font-extrabold ">K</h1>
-          <h1 className="text-white font-extrabold ">I</h1>
-          <h1 className="text-white font-extrabold ">N</h1>
-          <h1 className="text-white font-extrabold ">X</h1>
-          <h1 className="text-white font-extrabold ">O</h1>
-          <h1 className="text-white font-extrabold ">R</h1>
-          <h1 className="text-white font-extrabold ">I</h1>
+      <div className="h-[100%] w-[100%] gap-5 flex flex-col justify-center items-center">
+        <div className="flex w-[100%] flex-wrap justify-around text-[60px] font-[rubik] ">
+          <h1 className="text-white font-extrabold leading-none">K</h1>
+          <h1 className="text-white font-extrabold leading-none">I</h1>
+          <h1 className="text-white font-extrabold leading-none">N</h1>
+          <h1 className="text-white font-extrabold leading-none">X</h1>
+          <h1 className="text-white font-extrabold leading-none">O</h1>
+          <h1 className="text-white font-extrabold leading-none">R</h1>
+          <h1 className="text-white font-extrabold leading-none">I</h1>
         </div>
-        <div className="flex flex-col justify-center items-center text-center text-white opacity-70 ">
-          <h2>
-            Combining <b>creativity, numbers</b> and <b>code</b> to make things happen 🚀
+        <div className="gap-5 w-[90%] flex flex-col justify-center items-center text-center text-white ">
+          <h2 className="opacity-100">
+            Combining <b>code</b> and <b>creativity</b> to make things happen 🚀
           </h2>
-          {/* <p>
+          <div className="">
+            <Button variant="mainButton">Contact Me</Button>
+            {/* <p>
             Hi! I am Kinxori and <b>welcome</b> to my website! Developing myself as a
             <b> web developer</b> and <b>motion designer.</b> Take a deep dive into my work!
           </p> */}
-          <div className="landingPage-CTA">
-            <Button variant="mainButton">Contact Me</Button>
           </div>
         </div>
       </div>
