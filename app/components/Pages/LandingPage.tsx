f965b6f70c13374f1a3ee098f5c820a0135d6511
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-04 17:09:29 -0600

    updating content - landing page - 08

--- app/components/Pages/LandingPage.tsx
+++ app/components/Pages/LandingPage.tsx
@@ -3,18 +3,18 @@ import ButtonAnchor from "../CustomButton/AnchorButton";
 export default function LandingPage({ id }: { id: string }) {
   return (
     <article id={id} className="h-[100vh] w-[100%] mx-auto box-border p-10 overflow-hidden">
-      <div className="h-[100%] w-[100%] flex flex-col justify-center items-center">
-        <div className="flex w-[100%] flex-wrap justify-around text-[60px] font-[rubik] ">
-          <h1 className="text-white font-extrabold ">K</h1>
-          <h1 className="text-white font-extrabold ">I</h1>
-          <h1 className="text-white font-extrabold ">N</h1>
-          <h1 className="text-white font-extrabold ">X</h1>
-          <h1 className="text-white font-extrabold ">O</h1>
-          <h1 className="text-white font-extrabold ">R</h1>
-          <h1 className="text-white font-extrabold ">I</h1>
+      <div className="h-[100%] gap-3 w-[100%] flex flex-col justify-center items-center">
+        <div className="text-white leading-none font-extrabold text-[60px] items-end justify-around font-[rubik] flex w-[100%] ">
+          <h1>K</h1>
+          <h1>I</h1>
+          <h1>N</h1>
+          <h1>X</h1>
+          <h1>O</h1>
+          <h1>R</h1>
+          <h1>I</h1>
         </div>
-        <div className="gap-10 w-[90%] flex flex-col justify-center items-center text-center text-white ">
-          <h2 className="opacity-80 text-[16px] leading-[18px]">
+        <div className="gap-12 w-[90%] flex flex-col justify-center items-center text-center text-white ">
+          <h2 className="opacity-80 text-[16px]">
             Combining <b>code</b> and <b>creativity</b> to make things happen 🚀
           </h2>
           <ButtonAnchor
