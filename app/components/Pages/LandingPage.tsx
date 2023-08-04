1c09ee3a9cc957fa5129aacaccbdf2d2d33606ee
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-04 17:12:02 -0600

    updating content - landing page - 09

--- app/components/Pages/LandingPage.tsx
+++ app/components/Pages/LandingPage.tsx
@@ -5,13 +5,27 @@ export default function LandingPage({ id }: { id: string }) {
     <article id={id} className="h-[100vh] w-[100%] mx-auto box-border p-10 overflow-hidden">
       <div className="h-[100%] gap-3 w-[100%] flex flex-col justify-center items-center">
         <div className="text-white leading-none font-extrabold text-[60px] items-end justify-around font-[rubik] flex w-[100%] ">
-          <h1>K</h1>
-          <h1>I</h1>
-          <h1>N</h1>
-          <h1>X</h1>
-          <h1>O</h1>
-          <h1>R</h1>
-          <h1>I</h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            K
+          </h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            I
+          </h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            N
+          </h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            X
+          </h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            O
+          </h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            R
+          </h1>
+          <h1 className="hover:text-mainColor hover:scale-[1.05] transition-all duration-[.2s] ease-in-out">
+            I
+          </h1>
         </div>
         <div className="gap-12 w-[90%] flex flex-col justify-center items-center text-center text-white ">
           <h2 className="opacity-80 text-[16px]">
