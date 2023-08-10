ad93ec2043ed5de706ae565d70e606783bf62498
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-10 12:52:04 -0600

    adding alt description to skills logos

--- app/components/Pages/SkillsPage.tsx
+++ app/components/Pages/SkillsPage.tsx
@@ -34,47 +34,51 @@ export default function Skills({ id, isObserved }: { id?: string; isObserved?: s
         <hr className="my-10 border-white/50"></hr>
         <div className="text-[12px] font-bold underline decoration-mainColor gap-5 flex flex-wrap justify-evenly">
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={reactLogo} alt="" className="h-[50px] object-contain " />
+            <img src={reactLogo} alt="reactjs Logo" className="h-[50px] object-contain " />
             <span>React</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={javascriptLogo} alt="" className="h-[50px] object-contain " />
+            <img src={javascriptLogo} alt="javascript Logo" className="h-[50px] object-contain " />
             <span>JavaScript</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={typescriptLogo} alt="" className="h-[50px] object-contain " />
+            <img src={typescriptLogo} alt="typescript Logo" className="h-[50px] object-contain " />
             <span>TypeScript</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={htmlLogo} alt="" className="h-[50px] object-contain " />
+            <img src={htmlLogo} alt="html5 Logo" className="h-[50px] object-contain " />
             <span>HTML5</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={cssLogo} alt="" className="h-[50px] object-contain " />
+            <img src={cssLogo} alt="css3 Logo" className="h-[50px] object-contain " />
             <span>CSS3</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={tailwindcssLogo} alt="" className="h-[50px] object-contain " />
+            <img
+              src={tailwindcssLogo}
+              alt="tailwindcss Logo"
+              className="h-[50px] object-contain "
+            />
             <span>TailwindCSS</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={remixLogo} alt="" className="h-[50px] object-contain " />
+            <img src={remixLogo} alt="remix Logo" className="h-[50px] object-contain " />
             <span>Remix</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={mongoDBLogo} alt="" className="h-[50px] object-contain " />
+            <img src={mongoDBLogo} alt="mongoDB Logo" className="h-[50px] object-contain " />
             <span>MongoDB</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={prismaLogo} alt="" className="h-[50px] object-contain " />
+            <img src={prismaLogo} alt="prisma Logo" className="h-[50px] object-contain " />
             <span>Prisma</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={firebaseLogo} alt="" className="h-[50px] object-contain " />
+            <img src={firebaseLogo} alt="firebase Logo" className="h-[50px] object-contain " />
             <span>Firebase</span>
           </div>
           <div className="hover:scale-105 transition-all duration-25 ease-in-out gap-1 flex flex-col justify-center items-center h-[80px] w-[70px] ">
-            <img src={restAPILogo} alt="" className="h-[50px] object-contain " />
+            <img src={restAPILogo} alt="restAPI Logo" className="h-[50px] object-contain " />
             <span>Rest API</span>
           </div>
         </div>
