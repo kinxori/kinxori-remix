d1b3c262cbb88618899d26fb954c09223fd172e6
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-10 16:34:59 -0600

    testing window.scrollY for animations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,8 @@ export default function Index() {
   const [isAboutMePage, setAboutMePage] = useState(false);
   const [isSkillsPage, setSkillsPage] = useState(false);
   const [isFooterPage, setFooterPage] = useState(false);
-
-  console.log("booleans", { isLandingPage, isAboutMePage, isSkillsPage, isFooterPage });
+  const [isScrolled, setScrolled] = useState(0);
+  const [screenHeight, setScreenHeight] = useState(0);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -18,7 +18,7 @@ export default function Index() {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             const id = entry.target.getAttribute("id");
-            console.log("id", id);
+            // console.log("id", id);
             id === "landingPageID" ? setLandingPage(true) : setLandingPage(false);
             id === "aboutMePageID" ? setAboutMePage(true) : setAboutMePage(false);
             id === "skillsPageID" ? setSkillsPage(true) : setSkillsPage(false);
@@ -31,16 +31,46 @@ export default function Index() {
         threshold: 0.9,
       }
     );
-
     const elementsToObserver = document.querySelectorAll(".observe");
     elementsToObserver.forEach((element) => {
       observer.observe(element);
     });
-
     return () => {
       observer.disconnect();
     };
   }, []);
+
+  // console.log("booleans", { isLandingPage, isAboutMePage, isSkillsPage, isFooterPage });
+
+  // const handleScroll = () => {
+  //   const scrollHeigth = window.scrollY;
+  //   const screenHeigth = window.innerHeight;
+  //   setScrolled(scrollHeigth > screenHeigth / 2);
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
+  // const handleClick = () => {
+  //   window.scrollTo({ top: 0, behavior: "smooth" });
+  // };
+
+  // TEsting
+
+  const handleScroll = () => {
+    const scrollHeigth = window.scrollY;
+    const screenHeigth = window.innerHeight;
+    setScrolled(scrollHeigth);
+    setScreenHeight(screenHeigth);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  console.log("scroll position", { isScrolled, screenHeight });
 
   return (
     <article className="relative flex flex-col gap-10">
