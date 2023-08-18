3fe3c8b292c4c46b980c92f047f9fc31bb731428
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-18 11:23:02 -0600

    added resume screenshot to experience page

--- app/components/Pages/ExperiencePage.tsx
+++ app/components/Pages/ExperiencePage.tsx
@@ -1,27 +1,7 @@
-import { useState } from "react";
-import { Link } from "@remix-run/react";
 import projectsAsset from "~/assets/email-sent-asset.png";
-import AnchorButton from "~/components/CustomButton/AnchorButton";
-import elDiablito13Asset from "~/assets/el-diablito-13-website-ss.png";
-import musicMediaPlayerAsset from "~/assets/music-media-player-practice-ss.png";
-import reactLogo from "~/assets/Logos/react-color-logo.png";
-import javascriptLogo from "~/assets/Logos/javascript-color-logo.png";
-import typescriptLogo from "~/assets/Logos/typescript-color-logo.png";
-import htmlLogo from "~/assets/Logos/html-color-logo.png";
-import cssLogo from "~/assets/Logos/css-color-logo.png";
-import remixLogo from "~/assets/Logos/remix-color-logo.png";
-import mongoDBLogo from "~/assets/Logos/mongodb-color-logo.png";
-import prismaLogo from "~/assets/Logos/prisma-color-logo.png";
-import firebaseLogo from "~/assets/Logos/firebase-color-logo.png";
-import restAPILogo from "~/assets/Logos/rest-api-color-logo.png";
-import tailwindcssLogo from "~/assets/Logos/tailwindcss-png-logo-500x500-white.png";
-import viteJslogo from "~/assets/Logos/vite-js-color-logo.png";
-import githubLogo from "~/assets/Logos/github-white-logo.png";
-import technicalTest1 from "~/assets/technical-test-1-ss.png";
+import resumeAsset from "~/assets/resume-ss.png";
 
 export default function WebProjectsPage({ id, isObserved }: { id?: string; isObserved?: string }) {
-  const [idk, setIdk] = useState(<ElDiablito13 />);
-
   return (
     <section
       id={id}
@@ -39,229 +19,23 @@ export default function WebProjectsPage({ id, isObserved }: { id?: string; isObs
       <div className="box-border pl-10 pt-10 pr-10 flex flex-col">
         <div>
           <p className="font-[inter] font-normal text-justify ">
-            Explore a collection of my web projects and delve into the world of my development
-            practices. From crafting engaging user interfaces to crafting elegant code, these
-            projects showcase my journey as a web developer.
+            During my journey from Motion Designer to Web Developer, I've had the privilege of
+            diving into a vibrant array of projects. My path has led me to collaborate with local
+            and international teams, engage directly with clients in sales, and craft captivating
+            video ads from the comfort of my own home.
+          </p>
+          <p className="font-[inter] font-normal text-justify ">
+            I thrive on adapting swiftly and absorbing knowledge like a sponge, allowing me to
+            evolve seamlessly in this ever-changing landscape.
+          </p>
+          <p className="font-[inter] font-normal text-justify ">
+            Take a look at my career path and if you feel we are a good match. Let's connect and
+            explore the possibilities. Feel free to download my latest resume.
           </p>
         </div>
         <hr className="mt-10 border-white/50"></hr>
+        <img className="mt-10 rounded-[10px] " src={resumeAsset} />
       </div>
-      <section
-        id="carrouselProjects"
-        className="overflow-x-scroll w-[100vw] px-10 py-10 flex gap-5 items-start"
-      >
-        <ElDiablito13 />
-        <MusicMediaPlayer />
-        <TechnicalTest1 />
-        <TechnicalTest2 />
-      </section>
     </section>
-  );
-}
-
-function ElDiablito13() {
-  return (
-    <div
-      className="flex-shrink-0 snap-center hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-      border-mainColor w-[310px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
-    >
-      <div>
-        <img className="rounded-[10px] mb-5" src={elDiablito13Asset} />
-        <i className="text-[12px] text-mainColor">#Project</i>
-        <h3 className="text-mainColor font-bold text-[20px] text-left">El Diablito 13</h3>
-        <p className="text-[14px] ">
-          El Diablito 13's official website.{" "}
-          <Link to="" className="underline italic text-mainColor ">
-            Read more.
-          </Link>
-        </p>
-        <div className="flex gap-2 mt-2 flex-wrap w-[80%]">
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={reactLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={javascriptLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={htmlLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={cssLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={viteJslogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={githubLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={firebaseLogo} />
-          </div>
-        </div>
-      </div>
-      <AnchorButton
-        className="mt-5 ml-auto"
-        ariaLabel="Hyperlink to visit El Diablito 13's official website."
-        href="https://el-diablito-13-b8034.firebaseapp.com/"
-        target="_blank"
-        variant="mainButton"
-      >
-        Live Project&#160;&#160;
-        <i className="fa-solid fa-arrow-up-right-from-square text-[12px]"></i>
-      </AnchorButton>
-    </div>
-  );
-}
-
-function MusicMediaPlayer() {
-  return (
-    <div
-      className="  flex-shrink-0 snap-center hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-    border-mainColor w-[310px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
-    >
-      <div>
-        <img className="rounded-[10px] mb-5" src={musicMediaPlayerAsset} />
-        <i className="text-[12px] text-mainColor">#Practice</i>
-        <h3 className="text-mainColor font-bold text-[20px] text-left">Music Media Player</h3>
-        <p className="text-[14px] ">
-          Creating a music media player.{" "}
-          <Link to="" className="underline italic text-mainColor ">
-            Read more.
-          </Link>
-        </p>
-        <div className="flex gap-2 mt-2 flex-wrap w-[80%]">
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={reactLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={javascriptLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={htmlLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={tailwindcssLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={viteJslogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={githubLogo} />
-          </div>
-        </div>
-      </div>
-      <AnchorButton
-        className="mt-5 ml-auto"
-        ariaLabel="Hyperlink to visit El Diablito 13's official website."
-        href="https://kinxori.github.io/Music-Media-Player-PRACTICE/"
-        target="_blank"
-        variant="mainButton"
-      >
-        Live Project&#160;&#160;
-        <i className="fa-solid fa-arrow-up-right-from-square text-[12px]"></i>
-      </AnchorButton>
-    </div>
-  );
-}
-
-function TechnicalTest1() {
-  return (
-    <div
-      className="  flex-shrink-0 snap-center hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-    border-mainColor w-[310px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
-    >
-      <div>
-        <img className="rounded-[10px] mb-5" src={technicalTest1} />
-        <i className="text-[12px] text-mainColor">#TechnicalTest</i>
-        <h3 className="text-mainColor font-bold text-[20px] text-left">Contact Manager Feature</h3>
-        <p className="text-[14px] ">
-          Technical test for a Frontend Jr role.{" "}
-          <Link to="" className="underline italic text-mainColor ">
-            Read more.
-          </Link>
-        </p>
-        <div className="flex gap-2 mt-2 flex-wrap w-[80%]">
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={reactLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={javascriptLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={htmlLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={tailwindcssLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={viteJslogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={githubLogo} />
-          </div>
-        </div>
-      </div>
-      <AnchorButton
-        className="mt-5 ml-auto"
-        ariaLabel="Hyperlink to visit El Diablito 13's official website."
-        href="https://kinxori.github.io/technical-test-1/"
-        target="_blank"
-        variant="mainButton"
-      >
-        Live Project&#160;&#160;
-        <i className="fa-solid fa-arrow-up-right-from-square text-[12px]"></i>
-      </AnchorButton>
-    </div>
-  );
-}
-
-function TechnicalTest2() {
-  return (
-    <div
-      className="  flex-shrink-0 snap-center hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-    border-mainColor w-[310px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
-    >
-      <div>
-        <img className="rounded-[10px] mb-5" src={technicalTest1} />
-        <i className="text-[12px] text-mainColor">#TechnicalTest</i>
-        <h3 className="text-mainColor font-bold text-[20px] text-left">User's Details Modal</h3>
-        <p className="text-[14px] ">
-          Technical test for a Frontend Jr role.{" "}
-          <Link to="" className="underline italic text-mainColor ">
-            Read more.
-          </Link>
-        </p>
-        <div className="flex gap-2 mt-2 flex-wrap w-[80%]">
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={reactLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={javascriptLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={htmlLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={tailwindcssLogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={viteJslogo} />
-          </div>
-          <div className="h-8 w-8 overflow-hidden rounded-full bg-bgColor box-border p-[7px] ">
-            <img src={githubLogo} />
-          </div>
-        </div>
-      </div>
-      <AnchorButton
-        className="mt-5 ml-auto"
-        ariaLabel="Hyperlink to visit El Diablito 13's official website."
-        href="https://kinxori.github.io/technical-test-1/"
-        target="_blank"
-        variant="mainButton"
-      >
-        Live Project&#160;&#160;
-        <i className="fa-solid fa-arrow-up-right-from-square text-[12px]"></i>
-      </AnchorButton>
-    </div>
   );
 }
