082cc52a68d8b72248e161cd8fae62d10d26f31f
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-24 23:20:24 -0600

    music media player project - 05 - adding meta function with generic content

--- app/routes/music-media-player-project.tsx
+++ app/routes/music-media-player-project.tsx
@@ -2,7 +2,7 @@ import ScrollTopButton from "~/components/ScrollTopButton/ScrollTopButton";
 import Footer from "~/components/Pages/FooterPage";
 import elDiablito13Asset from "~/assets/music-media-player-practice-ss.png";
 import AnchorButton from "~/components/CustomButton/AnchorButton";
-import { LinksFunction } from "@remix-run/node";
+import { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import elDiablitoLogo from "~/assets/Logos/kinxori-diablito-route-logo.png";
 import { useState } from "react";
 import Button from "~/components/CustomButton/Button";
@@ -196,6 +196,64 @@ export const links: LinksFunction = () => {
       href: elDiablitoLogo,
       type: "image/png",
       sizes: "100x100",
+    },
+  ];
+};
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Music Media Player - Project" },
+    {
+      name: "description",
+      content: "Generic",
+    },
+    {
+      property: "og:title",
+      content: "El Diablito 13 - Project",
+    },
+    {
+      property: "og:description",
+      content:
+        "Discover the very essence that fuels its existence, delve into its unique features, and immerse yourself in the intricacies of this remarkable project. From concept to creation, join me as I unveil the story behind El Diablito 13 and its creative allure.",
+    },
+    {
+      property: "og:image",
+      content:
+        "https://el-diablito-13-b8034.firebaseapp.com/assets/New_Logo_Diablo-edited-20b357a7.png",
+    },
+    {
+      property: "og:url",
+      content: "https://kinxori.com/el-diablito-13-project",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      property: "og:site_name",
+      content: "Kinxori",
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "twitter:site",
+      content: "@kinxori",
+    },
+    {
+      name: "twitter:title",
+      content: "El Diablito 13 - Project",
+    },
+    {
+      name: "twitter:description",
+      content:
+        "Discover the very essence that fuels its existence, delve into its unique features, and immerse yourself in the intricacies of this remarkable project. From concept to creation, join me as I unveil the story behind El Diablito 13 and its creative allure.",
+    },
+    {
+      name: "twitter:image",
+      content:
+        "https://el-diablito-13-b8034.firebaseapp.com/assets/New_Logo_Diablo-edited-20b357a7.png",
     },
   ];
 };
