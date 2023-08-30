32697fe764d49b416f7697608c35fc61292afd7a
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-29 19:47:50 -0600

    updating user details project route - 01 - adding generic structure

--- app/routes/user-filter-project.tsx
+++ app/routes/user-filter-project.tsx
@@ -4,8 +4,9 @@ import ScrollTopButton from "~/components/ScrollTopButton/ScrollTopButton";
 import Footer from "~/components/Pages/FooterPage";
 import elDiablito13Asset from "~/assets/music-media-player-practice-ss.png";
 import NavBar from "~/components/NavBar/NavBar";
+import AnchorButton from "~/components/CustomButton/AnchorButton";
 
-export default function UserDetails() {
+export default function UserFilter() {
   return (
     <section className="font-[inter] bg-mainColor pt-[60px] overflow-y-scroll w-[100vw] flex flex-col text-white">
       <div className=" bg-bgColor flex-col flex p-10 box-border">
@@ -16,12 +17,33 @@ export default function UserDetails() {
             </h3>
             <i className="text-white/50 font-light text-[14px]">#TechnicalTest</i>
           </div>
-          <p className="text-justify mt-5">
-            Discover the very essence that fuels its existence, delve into its unique features, and
-            immerse yourself in the intricacies of this remarkable project. From concept to
-            creation, join me as I unveil the story behind El Diablito 13 and its creative allure.
-          </p>
-          <img className="rounded-[10px] mt-5" src={elDiablito13Asset} />
+          <div className="flex flex-col gap-5">
+            <img className="rounded-[10px]" src={elDiablito13Asset} />
+            <div className="flex flex-col">
+              <p className="text-justify">
+                Discover the very essence that fuels its existence, delve into its unique features,
+                and immerse yourself in the intricacies of this remarkable project. From concept to
+                creation, join me as I unveil the story behind El Diablito 13 and its creative
+                allure.
+              </p>
+              <div className="mt-5 ml-auto flex gap-2">
+                <AnchorButton
+                  variant="secButton"
+                  href="https://github.com/kinxori/technical-test-1"
+                  target="_blank"
+                >
+                  Github Repo
+                </AnchorButton>
+                <AnchorButton
+                  variant="mainButton"
+                  href="https://kinxori.github.io/technical-test-1/ "
+                  target="_blank"
+                >
+                  Live Page
+                </AnchorButton>
+              </div>
+            </div>
+          </div>
         </div>
         <hr className="border-white/50 my-10"></hr>
       </div>
