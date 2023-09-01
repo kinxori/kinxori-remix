24991723d119591df74d6d38a2ed374efee825d0
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-31 18:40:50 -0600

    updated contact manager route - adding content for Project Overview (rought cut)

--- app/routes/contact-manager-project.tsx
+++ app/routes/contact-manager-project.tsx
@@ -94,13 +94,37 @@ export default function ContactsManager() {
                 <h3 className="text-[20px] font-bold text-left ">
                   <b>Project Overview</b>
                 </h3>
-                <VideoLoader
-                  className="rounded-[10px]"
-                  autoPlay
-                  muted
-                  loop
-                  src="https://firebasestorage.googleapis.com/v0/b/myportfolio-70cb1.appspot.com/o/el-diablito-13-overview-project-asset.mp4?alt=media&token=f8dfd4cc-4fc4-4a00-b9d9-a66297626b38"
-                />
+                <p>
+                  Introducing the Contact Manager Project The "Contact Manager" project was
+                  developed as a technical test submission for a Junior Frontend Developer role.
+                  This React-based application showcases the required skills in creating a
+                  functional and user-friendly contact management tool. The Contact Manager empowers
+                  users to efficiently organize and handle their contacts, whether personal or
+                  business-related.
+                </p>
+                <h4 className="text-[16px] font-bold text-left ">Client and Target Audience</h4>
+                <p>
+                  The primary audience for the Contact Manager project is the hiring team evaluating
+                  my proficiency in frontend development. Additionally, the project demonstrates the
+                  ability to create a practical solution for individuals seeking an uncomplicated
+                  way to manage their contacts.
+                </p>
+                <span>
+                  🛑 Let's add maybe an image of a developer team/hiring manager. (not obligatory)
+                  Must be in 16:9 🛑
+                </span>
+                <h4 className="text-[16px] font-bold text-left ">Problem and Context</h4>
+                <p>
+                  As part of the technical test for the Junior Frontend Developer role, the Contact
+                  Manager project was created to address the challenge of building a responsive and
+                  feature-rich web application. The project's purpose is to demonstrate the grasp of
+                  React concepts, state management, form handling, and user interface design. By
+                  developing a functional contact management tool, I can showcases my ability to
+                  craft real-world applications that are both visually appealing and highly usable.
+                </p>
+                <span>
+                  🛑 Let's add a high resolution img focusing on the application. Must be in 16:9 🛑
+                </span>
               </div>
             )}
             {selectedSubject === 2 && (
