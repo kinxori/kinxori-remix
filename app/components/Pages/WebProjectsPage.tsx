8e41077225d98710deab9ab475bcfae3e872e696
kinxori <116584982+kinxori@users.noreply.github.com>
2023-09-12 20:19:21 -0600

    adding responsive for tablet at <projects page>

--- app/components/Pages/WebProjectsPage.tsx
+++ app/components/Pages/WebProjectsPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "@remix-run/react";
 import projectsAsset from "~/assets/projects-asset.png";
 import AnchorButton from "~/components/CustomButton/AnchorButton";
@@ -11,15 +10,10 @@ import javascriptLogo from "~/assets/Logos/javascript-color-logo.png";
 import typescriptLogo from "~/assets/Logos/typescript-color-logo.png";
 import htmlLogo from "~/assets/Logos/html-color-logo.png";
 import cssLogo from "~/assets/Logos/css-color-logo.png";
-import remixLogo from "~/assets/Logos/remix-color-logo.png";
-import mongoDBLogo from "~/assets/Logos/mongodb-color-logo.png";
-import prismaLogo from "~/assets/Logos/prisma-color-logo.png";
 import firebaseLogo from "~/assets/Logos/firebase-color-logo.png";
-import restAPILogo from "~/assets/Logos/rest-api-color-logo.png";
 import tailwindcssLogo from "~/assets/Logos/tailwindcss-png-logo-500x500-white.png";
 import viteJslogo from "~/assets/Logos/vite-js-color-logo.png";
 import githubLogo from "~/assets/Logos/github-white-logo.png";
-
 import nodejsLogo from "~/assets/Logos/nodejs-color-logo.png";
 
 export default function WebProjectsPage({ id, isObserved }: { id?: string; isObserved?: string }) {
@@ -28,18 +22,25 @@ export default function WebProjectsPage({ id, isObserved }: { id?: string; isObs
       id={id}
       className={`font-[inter] w-[100vw] flex flex-col justify-center items-center ${isObserved}`}
     >
-      <div className="bg-mainColor box-border p-10 w-[100%] relative overflow-hidden">
-        <h2 className="z-[10] customTextShadowMainColor1 relative font-[rubik] font-bold text-[50px] text-right">
+      <div className="bg-mainColor box-border p-10 w-[100%] relative overflow-hidden ">
+        <h2
+          className="z-[10] customTextShadowMainColor1 relative font-[rubik] font-bold text-[50px] text-left
+        ms:text-[60px]"
+        >
           Projects
         </h2>
         <img
           src={projectsAsset}
-          className="opacity-30 z-[0] absolute left-[-20%] bottom-[-100%] h-[400px] "
+          className="opacity-30 z-[0] absolute right-[-20%] bottom-[-100%] h-[400px] 
+          ms:right-[-10%] ms:h-[500px] ms:bottom-[-120%] "
         />
       </div>
       <div className="w-[100vw] flex flex-col">
-        <div className="box-border p-10 pb-0">
-          <p className=" font-normal text-justify ">
+        <div
+          className="box-border p-10 pb-0
+        ms:px-[15%] py-[10%]"
+        >
+          <p>
             Explore a collection of my web projects and delve into the world of my development
             practices. From crafting engaging user interfaces to crafting elegant code, these
             projects showcase my journey as a web developer.
@@ -48,7 +49,8 @@ export default function WebProjectsPage({ id, isObserved }: { id?: string; isObs
         </div>
         <section
           id="carrouselProjects"
-          className="overflow-x-scroll w-[100vw] p-10 flex gap-5 items-start"
+          className="overflow-x-scroll w-[100vw] p-10 flex gap-5 items-start
+          ms:px-[15%]  "
         >
           <ElDiablito13 />
           <MusicPlayer />
@@ -64,7 +66,9 @@ function ElDiablito13() {
   return (
     <article
       className="flex-shrink-0 hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-      border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
+      border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start
+       ms:p-10 ms:min-h-[590px]
+       "
     >
       <img
         alt="Screenshot of El Diablito 13 landing page website. At the left side of the screen you have the main copy of the landing page. Below that you have 2 call to action buttons. At the right side you have a drawing of a devil smoking a cigar with long hair."
@@ -127,7 +131,9 @@ function MusicPlayer() {
   return (
     <article
       className="  flex-shrink-0 hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-    border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
+    border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start 
+    ms:p-10 ms:min-h-[590px]
+    "
     >
       <img
         alt="Screenshot of Kinxori's project named Music Player. There's 2 panels, one showing the current song playing and a bunch of buttons to interact with the media. The second panel is displaying a list of songs as a playlist."
@@ -187,7 +193,9 @@ function ContactManager() {
   return (
     <article
       className="  flex-shrink-0 hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-    border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
+    border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start
+    ms:p-10 ms:min-h-[590px]
+    "
     >
       <img
         alt="Screenshot of Kinxori's project named Contact Manager Feature, which was a technical test for a Frontend Jr role where the main challenge was to have a feature to add, remove and filter user entries based of the type of contact."
@@ -247,7 +255,8 @@ function CallHistoryFilter() {
   return (
     <article
       className="  flex-shrink-0 hover:customShadowMainColor2 hover:scale-[1.01] transition-all duration-[.3s] 
-    border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start"
+    border-mainColor w-[100%] min-h-[510px] text-bgColor bg-white border-[2px] rounded-[10px] box-border p-5 flex flex-col justify-start 
+    ms:p-10 ms:min-h-[590px] "
     >
       <img
         alt="Screenshot of Kinxori's project named User's Details Modal, which was a technical test for a Frontend Jr role displaying an array of contacts filtered in a specific way."
