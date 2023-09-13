23dcaaa11a202543bd0159b2ecf296d18c7f21da
kinxori <116584982+kinxori@users.noreply.github.com>
2023-09-12 18:23:16 -0600

    updating <desktop navbar> - adding expandable modal for projects button - working

--- app/components/NavBar/DesktopNavBar.tsx
+++ app/components/NavBar/DesktopNavBar.tsx
@@ -1,33 +1,56 @@
 import { Link } from "@remix-run/react";
 import LinkButton from "~/components/CustomButton/LinkButton";
-import AnchorButton from "~/components/CustomButton/AnchorButton";
 import Button from "~/components/CustomButton/Button";
-// import LinkButton from "@src/components/Buttons/LinkButton";
-// import ButtonAnchor from "@src/components/Buttons/ButtonAnchor";
-// import resumeFilePDF from "@src/assets/FrontEnd-GustavoQuiroz-Resume-June-2023.pdf";
+import { useState } from "react";
 
 export default function DesktopNav() {
+  const [isClicked, setClicked] = useState(false);
+
   return (
     <nav className="fixed z-[1000] top-0 h-[80px] bg-bgColor border-b-[2px] border-mainColor w-screen flex justify-between items-center px-5 box-border ">
       <Link to="/" className="font-[rubik] text-[50px] font-bold    ">
         <h1>Kinxori</h1>
       </Link>
-      <div className="">
-        <div className="desktopNavBar-CTA-buttons">
-          <LinkButton variant="ghostButton" to="/">
-            Home
-          </LinkButton>
-          <LinkButton variant="ghostButton" to="/about">
-            About
-          </LinkButton>
-          <LinkButton variant="ghostButton" to="/contact">
-            Contact
-          </LinkButton>
-          <Button variant="ghostButton">Projects</Button>
-          <LinkButton variant="ghostButton" to="/resumePDF" download>
-            Download CV
-          </LinkButton>
-        </div>
+      <div className="h-full flex items-center">
+        <LinkButton variant="ghostButton" to="/">
+          Home
+        </LinkButton>
+        <LinkButton variant="ghostButton" to="/about">
+          About
+        </LinkButton>
+        <LinkButton variant="ghostButton" to="/contact">
+          Contact
+        </LinkButton>
+        <button
+          className="font-bold text-[14px] pl-[20px] py-[10px] h-full relative flex items-center"
+          onClick={() => setClicked(!isClicked)}
+        >
+          Projects&#160;&#160;
+          <i
+            className={`fa-solid fa-chevron-down transition-all 
+            ${isClicked ? "rotate-0" : "rotate-180"}`}
+          ></i>
+          <div
+            className={`absolute top-[80px] transition-all right-0 text-right bg-bgColor border-b-[2px] border-l-[2px] border-r-[2px] border-mainColor rounded-b-[10px] overflow-hidden  min-w-[200px] 
+            ${isClicked ? "p-0 h-[0px] opacity-0 " : "p-5 h-[220px] "} `}
+          >
+            <LinkButton variant="ghostButton" to="/el-diablito-13-project">
+              El Diablito 13
+            </LinkButton>
+            <LinkButton variant="ghostButton" to="/music-player-project">
+              Music Player
+            </LinkButton>
+            <LinkButton variant="ghostButton" to="/contact-manager-project">
+              Contact Manager
+            </LinkButton>
+            <LinkButton variant="ghostButton" to="/call-history-filter-project">
+              Call History Filter
+            </LinkButton>
+          </div>
+        </button>
+        <LinkButton variant="ghostButton" to="/resumePDF" download={true} target="_blank">
+          Download CV
+        </LinkButton>
       </div>
     </nav>
   );
