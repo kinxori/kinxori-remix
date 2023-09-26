f0e656c2d0290bbb168b36e61c74dbbff929850d
kinxori <116584982+kinxori@users.noreply.github.com>
2023-09-25 23:11:20 -0600

    adding 404 go back logic navigation

--- app/components/Pages/Error404Page.tsx
+++ app/components/Pages/Error404Page.tsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "@remix-run/react";
 import img404 from "~/assets/404Assets.png";
 import LinkButton from "~/components/CustomButton/LinkButton";
 
 export default function Error404Page() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <section className=" box-border pt-[60px] ">
       <div className="m-10 flex flex-col justify-center items-center">
         <img src={img404} alt="" className="object-contain w-[70%] my-10" />
         <h2 className="font-bold">Oops! Something went wrong 😔</h2>
-        <LinkButton to="/" variant="mainButton" className="my-5">
+        <LinkButton onClick={handleGoBack} to="" variant="mainButton" className="my-5">
           <i className="fa-solid fa-arrow-left"></i>&#160;&#160;Go Back
         </LinkButton>
       </div>
