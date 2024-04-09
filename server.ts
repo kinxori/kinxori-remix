492291e8bd7b2e4204ec460e6a32fe5e15c3190f
kinxori <gustavoq26@gmail.com>
2024-04-08 23:50:03 -0600

    removing <source-map-support> from server.ts -testing deploy

--- server.ts
+++ server.ts
@@ -1,10 +1,8 @@
 import * as build from "@remix-run/dev/server-build";
 import { createRequestHandler } from "@netlify/remix-adapter";
 import { installGlobals } from "@remix-run/node";
-import sourceMapSupport from "source-map-support";
 
 installGlobals();
-sourceMapSupport.install();
 
 export const handler = createRequestHandler({
   build,
